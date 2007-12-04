6ef269cca0e390178394fe3f15e17d0681653189
supertriceratops <supertriceratops@250d688f-3d3f-0410-b993-179eff13b859>
2007-12-04 05:43:38 +0000

    Added code to print out threshold trials for gnuplot

--- trunk/SimFind.cpp
+++ trunk/SimFind.cpp
@@ -18,10 +18,14 @@ mysqlpp::Connection *g_pdbcon2 = NULL;
 bool g_bMac = false;
 int  g_nSumToler = 5;
 int  g_nMaxCoeff = 5;
+bool g_bReportAll = false;
+// Collected statistics
 int  g_nRows = 0;
 int  g_nKeyHits = 0;
 int  g_nSumHits = 0;
 int  g_nIdentical = 0;
+int  g_nDataSize = 0;
+int  g_nCollisions = 0;
 
 #define MIN_KEY_TOL   1
 #define MIN_SUM_TOL   1
@@ -32,6 +36,17 @@ int  g_nIdentical = 0;
 
 /////////////////////////////////////////////////////////////////////////////
 // SimFind: main() and subroutines
+
+void ResetCounters()
+{
+	g_nRows = 0;
+	g_nKeyHits = 0;
+	g_nSumHits = 0;
+	g_nIdentical = 0;
+	g_nDataSize = 0;
+	g_nCollisions = 0;
+}
+
 
 bool AreRowsSameFile(mysqlpp::Row& row1, mysqlpp::Row& row2)
 {
@@ -99,16 +114,21 @@ void FindSimilarities(FILE* fp, char* szTable, mysqlpp::Row& row, bool bFindForw
 			// Files are similar... report
 			g_nSumHits++;
 			// Print current "row" for the first time
-			if (!bAddedHeader)
+			if (!bAddedHeader && g_bReportAll)
 			{
 				fprintf(fp, "%s\n", row.raw_data(0));
 				bAddedHeader = true;
 			}
+			
+			// Count hash key collisions
+			if (strcmp(row.raw_data(COL_HASH), rowsim.raw_data(COL_HASH)) == 0)
+				g_nCollisions++;
 
 			// Print found similarity
 			if ( (nDist == 0) && AreRowsSameFile(row, rowsim) )
 			{
-				fprintf(fp, "  <same> %s\n", rowsim.raw_data(0));
+				if (g_bReportAll)
+					fprintf(fp, "  <same> %s\n", rowsim.raw_data(0));
 				/* // This code removes identical files, should that be desired
 				char szFilePath[MAX_PATH];
 				sprintf(szFilePath, "%s%s", row.raw_data(1), row.raw_data(0));
@@ -116,7 +136,7 @@ void FindSimilarities(FILE* fp, char* szTable, mysqlpp::Row& row, bool bFindForw
 				*/
 				g_nIdentical++;
 			}
-			else
+			else if (g_bReportAll)
 				fprintf(fp, "  %6d %s\n", nDist, rowsim.raw_data(0));
 		}
 	}
@@ -177,6 +197,7 @@ void FindSimilaritiesForAll(FILE* fp, char* szTable)
 			if ( (g_nRows % 100 == 0) && g_nRows )
 				printf(" %d", g_nRows);
 			g_nRows++;
+			g_nDataSize += atoi(row.raw_data(COL_SIZE));
 		}
 	}
 	catch (const mysqlpp::EndOfResults& ){} // thrown when no more rows
@@ -188,6 +209,7 @@ int main(int argc, char* const argv[])
 	char szSimFindIni[MAX_PATH];
 	char szTable[MAX_PATH];
 	char szOutFile[MAX_PATH];
+	char szStatFile[MAX_PATH];
 	char szMatchFile[MAX_PATH];
 	
 #ifdef __APPLE__
@@ -212,6 +234,7 @@ int main(int argc, char* const argv[])
 
 	if ((fscanf(fp, "Table=%s\n", szTable) != 1) ||
 		(fscanf(fp, "OutFile=%s\n", szOutFile) != 1) ||
+		(fscanf(fp, "StatFile=%s\n", szStatFile) != 1) ||
 		(fscanf(fp, "MatchFile=%s\n", szMatchFile) != 1) ||
 		(fscanf(fp, "Tolerance=%d\n", &g_nSumToler) != 1) ||
 		(fscanf(fp, "MaxTagCoeff=%d", &g_nMaxCoeff) != 1) )
@@ -228,6 +251,15 @@ int main(int argc, char* const argv[])
 		printf("main: failed to open '%s'\n", szOutFile);
 		return false;
 	}
+	
+	// Open statt file for writing
+	FILE* fp2 = fopen(szStatFile, "wt");
+	if (fp2 == NULL)
+	{
+		fclose(fp);
+		printf("main: failed to open '%s'\n", szStatFile);
+		return false;
+	}
 
 	// Open two needed db connections
 	g_pdbcon1 = new mysqlpp::Connection(mysqlpp::use_exceptions);
@@ -235,18 +267,30 @@ int main(int argc, char* const argv[])
 	g_pdbcon1->connect(MYSQL_DATABASE, MYSQL_HOST, MYSQL_USER, MYSQL_PASS);
 	g_pdbcon2->connect(MYSQL_DATABASE, MYSQL_HOST, MYSQL_USER, MYSQL_PASS);
 
-	// Find file similarities
-	if (strlen(szMatchFile) > 1)
-		FindSimilaritiesForOne(fp, szTable, szMatchFile);
-	else
-		FindSimilaritiesForAll(fp, szTable);
+	fprintf(fp2, "# Table = '%s'\n", szTable);	
+	fprintf(fp2, "# TOLERANCE  KEYHITS   SUMHITS \n");
+	int anTols[] = { 10, 25, 50, 100, 250, 500, 1000, 2500 };
+	for (int i = 0; i < 8; i++)
+	{
+		ResetCounters();
+		g_nSumToler = anTols[i];
+		// Find file similarities
+		if (strlen(szMatchFile) > 1)
+			FindSimilaritiesForOne(fp, szTable, szMatchFile);
+		else
+			FindSimilaritiesForAll(fp, szTable);
 
-	fprintf(fp, "\nRows in Table    = %d", g_nRows);
-	fprintf(fp, "\nHashKey Matches  = %d", g_nKeyHits);
-	fprintf(fp, "\nSumTable Matches = %d", g_nSumHits);
-	fprintf(fp, "\nIdentical Files  = %d", g_nIdentical);
+		fprintf(fp2, "%8d  %8d  %8d\n", g_nSumToler, g_nKeyHits, g_nSumHits);
+	}
+	
+	fprintf(fp2, "\n# Rows = %d\n", g_nRows);
+	fprintf(fp2, "# Total Size = %d\n", g_nDataSize);
+	fprintf(fp2, "# Collisions = %d\n", g_nCollisions);
+	fprintf(fp2, "# Identical = %d\n", g_nIdentical);
+
 
 	fclose(fp);
+	fclose(fp2);
 	delete g_pdbcon1;
 	delete g_pdbcon2;
 	return 0;
